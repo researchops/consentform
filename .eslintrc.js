737c7a9bf5d81d101f8889a709d3215b76e754db
Gorpalicious <porcelainsoul@hotmail.com>
2020-02-15 23:07:57 +0000

    latest copy changes @ 15/02/2020

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  extends: [
-    'standard',
-    'prettier',
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:security/recommended',
-  ],
-  plugins: ['jest', 'security'],
-  env: {
-    'jest/globals': true,
-  },
-  rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-  },
+	extends: [
+		'standard',
+		'prettier',
+		'eslint:recommended',
+		'plugin:import/recommended',
+		'plugin:security/recommended',
+	],
+	plugins: ['jest', 'security', 'prettier'],
+	env: {
+		'jest/globals': true,
+	},
+	rules: {
+		'comma-dangle': ['error', 'always-multiline'],
+	},
 }
--- routes/agreement-1/agreement-1.controller.js
+++ routes/agreement-1/agreement-1.controller.js
@@ -4,79 +4,77 @@ const nodePandoc = require('node-pandoc')
 const i18n = require('i18n')
 
 var callback = (err, result) => {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log('done conversion')
-  }
+	if (err) {
+		console.error(err)
+	} else {
+		console.log('done conversion')
+	}
 }
 
 function getRandomString() {
-  return Math.random()
-    .toString()
-    .split('.')[1]
-    .slice(0, 8)
+	return Math.random()
+		.toString()
+		.split('.')[1]
+		.slice(0, 8)
 }
 
 const startHtml = `<div style="display: none">start of agreement</div>`
 
 // make first letter lowercase and delete trailing periods
 const lowerCaseFirstLetter = s =>
-  s && s.length > 0 ? s[0].toLowerCase() + s.slice(1) : s
+	s && s.length > 0 ? s[0].toLowerCase() + s.slice(1) : s
 
 const stripTrailingPeriods = s =>
-  s && s.length > 0 ? s.replace(/\.*\s*$/, '') : s
+	s && s.length > 0 && typeof s == 'string' ? s.replace(/\.*\s*$/, '') : s
 
 const changeToPhrase = key =>
-  !key.includes('partner_department') && !key.includes('researcher')
+	!key.includes('partner_department') && !key.includes('researcher')
 
 module.exports = app => {
-  const name = 'agreement-1'
-  const route = routeUtils.getRouteByName(name)
+	const name = 'agreement-1'
+	const route = routeUtils.getRouteByName(name)
 
-  routeUtils.addViewPath(app, path.join(__dirname, './'))
+	routeUtils.addViewPath(app, path.join(__dirname, './'))
 
-  app.get(route.path, (req, res) => {
-    var nextRoute = getNextRoute(name).path
-    var randomString = getRandomString()
-    var docxFilename = 'agreement-' + randomString + '.docx'
+	app.get(route.path, (req, res) => {
+		var nextRoute = getNextRoute(name).path
+		var randomString = getRandomString()
+		var docxFilename = 'agreement-' + randomString + '.docx'
 
-    const data = routeUtils.getViewData(req, {}).data
-    var queryParams = {}
-    Object.keys(data)
-      .filter(key => key !== '_csrf' && data[`${key}`] !== '')
-      .forEach(key => {
-        if (changeToPhrase(key)) {
-          data[`${key}`] = lowerCaseFirstLetter(
-            stripTrailingPeriods(data[`${key}`]),
-          )
-        } else if (key.includes('partner_department')) {
-          data[`${key}`] = stripTrailingPeriods(data[`${key}`])
-        }
-        queryParams[`${key}`] = data[`${key}`]
-      })
+		const data = routeUtils.getViewData(req, {}).data
+		var queryParams = {}
+		Object.keys(data)
+			.filter(key => key !== '_csrf' && data[`${key}`] !== '')
+			.forEach(key => {
+				if (changeToPhrase(key)) {
+					data[`${key}`] = lowerCaseFirstLetter(stripTrailingPeriods(data[`${key}`]))
+				} else if (key.includes('partner_department')) {
+					data[`${key}`] = stripTrailingPeriods(data[`${key}`])
+				}
+				queryParams[`${key}`] = data[`${key}`]
+			})
 
-    res.render(
-      name + `-${i18n.getLocale(req)}`,
-      {
-        data,
-        nextRoute: nextRoute,
-        docxFilename: docxFilename,
-      },
-      function(err, html) {
-        if (err) {
-          console.log(err)
-        }
-        const startIndex = html.indexOf(startHtml) + startHtml.length
-        const endIndex = html.indexOf('</main>')
-        const htmlDoc = html.slice(startIndex, endIndex)
-        // nodePandoc(
-        //   htmlDoc,
-        //   '-f html -t docx -o public/documents/' + docxFilename,
-        //   callback,
-        // )
-        res.send(html)
-      },
-    )
-  })
+		res.render(
+			name + `-${i18n.getLocale(req)}`,
+			{
+				data,
+				nextRoute: nextRoute,
+				docxFilename: docxFilename,
+			},
+			function(err, html) {
+				if (err) {
+					console.log(err)
+				}
+				const startIndex = html.indexOf(startHtml) + startHtml.length
+				const endIndex = html.indexOf('</main>')
+				const htmlDoc = html.slice(startIndex, endIndex)
+				// nodePandoc(
+				//   htmlDoc,
+				//   '-f html -t docx -o public/documents/' + docxFilename,
+				//   callback,
+				// )
+				res.send(html)
+			},
+		)
+	})
 }
